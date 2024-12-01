255ecd41c53d764c1306fc5cb5846194b52a2891
Muhammad Shoaib <muhammadshoaib.bscsf21@iba-suk.edu.pk>
2024-12-01 11:31:38 +0500

    helo

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,14 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB connected successfully');
+    const mongoURI = `mongodb+srv://${process.env.MONGO_USERNAME}:${encodeURIComponent(process.env.MONGO_PASSWORD)}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=DigiPlant`;
+    await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log('MongoDB connected');
   } catch (err) {
-    console.error(err.message);
-    process.exit(1); // Exit on failure
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
   }
 };
 
